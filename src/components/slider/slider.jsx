c991dbfbd00ff02f3083759b4220f83d745117fb
Valeriy <KulikovVR@gmail.com>
2021-10-29 22:49:08 +0400

    Сверстал слайдер на трех брейкпойнтах

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -8,16 +8,21 @@ function Slider() {
         <p className="slider__tittle-text slider__tittle-text--first-slide">Лига Банк</p>
         <p className="slider__text slider__text--first-slide">Кредиты на любой случай</p>
         <a className="slider__button" href="#">Рассчитать кредит</a>
-        <picture className="slider__image-wrapper">
-          <source media="(min-width: 768px)" srcSet={'img/black-card-tab.png'}/>
-          <img className="slider__image slider__image--black-card" src={'img/black-card.png'} alt="Примерный вид черной карточки банка"/>
+        <picture className="slider__black-card-wrapper">
+          <source media="(min-width: 1024px)" srcSet={'img/black-card-desktop.png'} width="340" height="242"/>
+          <source media="(min-width: 768px)" srcSet={'img/black-card-tab.png'} width="256" height="172"/>
+          <img className="slider__black-card" src={'img/black-card.png'} alt="Примерный вид черной карточки банка"/>
         </picture>
-        <img className="slider__image slider__image--white-card" src={'img/white-card.png'} alt="Примерный вид белой карточки банка"/>
+        <picture className="slider__white-card-wrapper">
+          <source media="(min-width: 1024px)" srcSet={'img/white-card-desktop.png'} width="340" height="242"/>
+          <img className="slider__white-card" src={'img/white-card.png'} width="256" height="172" alt="Примерный вид белой карточки банка"/>
+        </picture>
       </div>
       <div className="slider__slide slider__slide--second">
         <p className="slider__tittle-text">Лига Банк</p>
         <p className="slider__text">Ваша уверенность в&nbsp;завтрашнем дне</p>
         <picture className="slider__image-wrapper">
+          <source media="(min-width: 1024px)" srcSet={'img/man-in-suit-desktop.jpg'}/>
           <source media="(min-width: 768px)" srcSet={'img/man-in-suit-tab.jpg'}/>
           <img className="slider__image" src={'img/man-in-suit.jpg'} alt="Мужчина в деловом костюме"/>
         </picture>
@@ -27,6 +32,7 @@ function Slider() {
         <p className="slider__text">Всегда рядом</p>
         <a className="slider__button slider__button--blue" href="#">Найти отделение</a>
         <picture className="slider__image-wrapper">
+          <source media="(min-width: 1024px)" srcSet={'img/girl-with-card-desktop.jpg'}/>
           <source media="(min-width: 768px)" srcSet={'img/girl-with-card-tab.jpg'}/>
           <img className="slider__image" src={'img/girl-with-card.jpg'} alt="Девушка держит карту банка"/>
         </picture>
